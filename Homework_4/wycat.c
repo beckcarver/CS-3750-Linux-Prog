6df69a8ba64121ff145e1bb33d6c31b85a7e9d7e
beckcarver <sonicboomrbd@gmail.com>
2023-02-21 10:53:40 -0700

    psuedocode completed

---
+++ Homework_4/wycat.c
@@ -0,0 +1,17 @@
+/* 
+ * wycat.c
+ * Author: Beckham Carver
+ * Date: Feb 21, 2023
+ * 
+ * COSC 3750, Homework 4
+ *
+ * Modified version of 'cat' utility
+ *
+ *
+ */
+
+int main(int argc, char* argv[])
+    const int BUFFSIZE = 4096;
+    
+
+
