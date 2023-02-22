d6c106202a1c811039c77442a21370701a95f74b
beckcarver <sonicboomrbd@gmail.com>
2023-02-21 18:59:39 -0700

    file read appears to work

--- Homework_4/wycat.c
+++ Homework_4/wycat.c
@@ -12,11 +12,55 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <strcmp.h>
+#include <string.h>
+#include <errno.h>
 
 
 // similar getline loop from man getline(3) but it never worked so I started
 // fresh- and now they look nearly identical again
+int intoOut(int buffsize);
+
+int main(int argc, char* argv[]) {
+    const int BUFFSIZE = 4096;
+    if(argc <= 1) {
+        intoOut(BUFFSIZE);
+    }
+    else {
+        
+        // debug
+        for (int i = 1; i < argc; i++) {
+            printf(argv[i]);
+            printf("  ");
+        }
+        printf("\n end of args \n");
+
+
+        for (int i = 1; i < argc; i++) {
+         
+            FILE *stream = fopen(argv[i], "r");
+            
+            if(!strcmp(argv[i], "-")) {
+                intoOut(BUFFSIZE);
+            }
+            else if (stream != NULL) {
+                // file is readable
+                printf("\n reading file \n");
+                char *buffer = malloc(BUFFSIZE);
+                size_t read = fread(buffer, sizeof(char), BUFFSIZE, stream);
+                fwrite(buffer, sizeof(char), read, stdout);
+                free(buffer);
+                fclose(stream);
+            }
+            else {
+                // file unreadable
+                char *errInput = argv[i];
+                perror(errInput);
+            }        
+        }
+    }
+    return 0;
+}
+
 int intoOut(int buffsize) {
     char *buffer = malloc(buffsize);
     size_t len = buffsize;
@@ -27,25 +71,7 @@ int intoOut(int buffsize) {
     }
     
     free(buffer);
-    return 1;
+    return 0;
 }
 
-int main(int argc, char* argv[]) {
-    const int BUFFSIZE = 4096;
-    
-    if(argc <= 1) {
-        intoOut(BUFFSIZE);
-     }
-    
-    for (int i = 1; i < argc; i++) {
 
-        if(strcmp(argv[i], '-')) {
-            intoOut(BUFFSIZE);
-        }
-        else if () {
-            // read into buffer
-        }
-
-    }
-
-}
