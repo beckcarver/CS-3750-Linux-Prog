9028040765b085ca8ea1eb395787cc306a91a846
beckcarver <sonicboomrbd@gmail.com>
2023-03-07 22:56:18 -0700

    approaching final

--- Homework_5/wyls.c
+++ Homework_5/wyls.c
@@ -21,13 +21,16 @@
 #include <pwd.h>
 #include <unistd.h>
 
+#define PATH_MAX 4096
+#define FNAME_MAX 256
+
 // compiler complains about strchr getting a char**, but this is intended
 // ncompatible-pointer-typesbecause argv[] is an array of char*, and we are pointing to the inner char*
 // program fails without &
 
 
 int process_dir(char* drname, int* flags);
-void process_entry(char *file, int* flags);
+void process_entry(char* fpath, char* fname, int* flags);
 void print_perm(mode_t mode); 
 
 int main(int argc, char* argv[]) { 
@@ -38,12 +41,14 @@ int main(int argc, char* argv[]) {
     // searches for options until FIRST argument without '-' is found
     while(pstop < argc && argv[pstop][0] == '-') {
         pstop++;
-        if(strchr(&argv[pstop], 'n')) { 
+        char opt[] = &argv[pstop][1];
+        if(strchr(opt, 'n')) { 
             flags[0] = 1;
         }
-        if(strchr(&argv[pstop], 'h')) {
+        if(strchr(opt, 'h')) {
             flags[1] = 1;
         }
+        printf(" %c %c\n", (char)flags[0], (char)flags[1]);
     }
     printf("pstop = %d\n", pstop);
     
@@ -51,18 +56,30 @@ int main(int argc, char* argv[]) {
 
     // if no additional arguments
     if (pstop >= argc) {
-        procDir(".", flags);
+        process_dir(".", flags);
         return 0;
     }
-
 
     // starting from end of option arguments, checks if directory 
     // or a file and calls functions accordingly, until end of arguments
     int k = pstop;
     do {
         printf("remaining args, k = %d\n", k);
-        if (
-        
+        char* fpath = realpath(argv[k], NULL);
+        struct stat spath;
+
+        // check if valid path at al
+        if (stat(fpath, &spath) != 0) {  
+            perror(strcat(argv[k], " invalid argument"));
+        }
+        // check if directory
+        else if (S_ISDIR(spath.st_mode)) {
+            process_dir(argv[k], flags); 
+        }
+        // not directory, and is a valid path
+        else {
+            process_entry(fpath, argv[k], flags);
+        }
         
         k++;
     } while (k < argc);    
@@ -72,7 +89,7 @@ int main(int argc, char* argv[]) {
 }
 
 
-int process_dir(char* drname, int* flags) {
+int process_dir(char *drname, int* flags) {
     DIR *dr;
     struct dirent *entry;
     dr = opendir(drname);
@@ -81,7 +98,8 @@ int process_dir(char* drname, int* flags) {
         return -1;
     }
     while((entry = readdir(dr)) != NULL) {
-        process_entry(entry->dnname, flags);
+        char* fpath = realpath(entry->d_name, NULL);
+        process_entry(fpath, entry->d_name, flags);
     }
     return 0;
 }
@@ -98,3 +116,49 @@ void print_perm(mode_t mode) {
     printf((mode & S_IWOTH) ? "w" : "-");
     printf((mode & S_IXOTH) ? "x" : "-");
 }
+
+void process_entry(char* fpath, char* fname, int* flags) {
+    // initialize stat, passwd, and group structires (not passed because
+    // passing from directory function is unwieldy)
+    struct stat spath;
+    stat(fpath, &spath);
+    struct passwd *pw = getpwuid(spath.st_uid);
+    struct group *gr = getgrgid(spath.st_gid);
+
+
+    // print permissions (always)
+    print_perm(spath.st_mode);
+
+    // print user/grp id's or names (flag dependent)
+    if (flags[0] == 1) {
+        printf(" %10d %10d", spath.st_uid, spath.st_gid);
+    }
+    else {
+        printf(" %10s %10s", pw->pw_name, gr->gr_name);
+    }
+
+    // print size bytes or human readable (flag dependent)
+    if (flags[1] == 1) {
+        double size = (double)spath.st_size;
+        if (size<1024){printf(" %10lf.0", size);}
+        else if (size<1024*1024){printf(" %10f.1K", size/1024.0);}
+        else if (size<1024*1024*1024){printf(" %10lf.1M", size/1024.0/1024.0);}
+        else {printf(" %10lf.1G", size/1024.0/1024.0/1024.0);} 
+    }
+    else {
+        printf(" %10ld", spath.st_size);
+    }
+
+    // print date (always)
+    char *date_format = " %b %d %H:%M";
+    if (time(NULL) - spath.st_mtime > 180 * 24 * 60 * 60) {
+        date_format = " %b  %d %Y";
+    }
+    struct tm *tm = localtime(&spath.st_mtime);
+    char date[64];
+    strftime(date, sizeof(date), date_format, tm);
+    printf(" %-s", date);
+
+    // print filename given
+    printf(" %-s\n", fname);
+} 
