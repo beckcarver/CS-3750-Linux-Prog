6fa8c17451b9074b288e0b0672232846b4f42edf
beckcarver <sonicboomrbd@gmail.com>
2023-04-12 23:57:33 -0600

    finished?

--- Homework_7/wytalkC.c
+++ Homework_7/wytalkC.c
@@ -12,9 +12,50 @@
 #include "socketfun.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <errno.h>
 
 #define PORT 51100
 
 
+int readConnection(int connect);
 
+int main(int argc, char* argv[]) {
+	if (argc != 2) {
+		printf("incorrect number of arguments");
+		return -1;
+	}
 
+	int connect = request_connection(argv[1], PORT);
+	if (connect < 3) {
+		printf("connection failed");
+		return -1;
+	}
+	
+	char sendbuf[1024];
+	
+	while(fgets(sendbuf, sizeof(sendbuf), stdin) != NULL) {
+		if(write(connect, sendbuf, sizeof(sendbuf)) == -1) {
+			perror("failed to write/send");
+			break;
+		}
+		sendbuf[0] = '\0';
+		if (readConnection(connect)) {break;}
+	}
+	
+	close(connect);
+
+	return 0;
+}
+
+int readConnection(int connect) {
+	char echo;
+	while(echo != '\n') {
+		// if zero bytes read, or error (-1) is returned
+		if (read(connect, &echo, 1) <= 0) {	
+			perror("zero/error bytes read");
+			return 1;
+		}
+		printf("%c", echo);
+	}
+	return 0;
+}
--- Homework_7/wytalkD.c
+++ Homework_7/wytalkD.c
@@ -12,9 +12,72 @@
 
 #include "socketfun.h"
 #include <unistd.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <errno.h>
+
+#define PORT 51100
+
+
+int readConnection(int connect);
+
+int main(int argc, char *argv[]) {
+	errno = 0;
+	char hostname[256];
+
+	if (gethostname(hostname, sizeof(hostname))) {
+		perror("failed to get hostname");
+		return -1;
+	}
+
+	printf("hostname is %s \n", hostname);
+	
+	int socket = serve_socket(hostname, PORT); 		
+	if (socket < 3) {
+		perror("serve_socket failed");
+		return -1;
+	}
+
+	int connect = accept_connection(socket); 
+	if (connect < 3) {
+		perror("accept_connection failed");
+		return -1;	
+	}
+
+	printf("connection established: host %s, socket %d, connection %d \n",
+			hostname, socket, connect);
+	
+	char sendbuf[1024];
+	readConnection(connect);
+
+	while(fgets(sendbuf, sizeof(sendbuf), stdin) != NULL) {
+		if(write(connect, sendbuf, sizeof(sendbuf)) == -1) {
+			perror("error in sending/writing data");
+			break;
+		}
+		sendbuf[0] = '\0';
+		if (readConnection(connect)) {break;}
+	}
+	
+	close(connect);
+	
+	return 0;
+}
 
 
 
+int readConnection(int connect) {
+	char echo;
+	while(echo != '\n') {
+		// if zero bytes read, or error (-1) is returned
+		if (read(connect, &echo, 1) <= 0) {	
+			perror("zero bytes/error returns in read");
+			return 1;
+		}
+		printf("%c", echo);
+	}
+	return 0;
+}
 
 
 
