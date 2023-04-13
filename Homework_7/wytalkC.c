b4732f0da8c3b0459b016a11f5276ebcbcb06791
beckcarver <sonicboomrbd@gmail.com>
2023-04-12 20:54:22 -0600

    -a

---
+++ Homework_7/wytalkC.c
@@ -0,0 +1,20 @@
+/*
+ * wytalkC.c
+ * Author: Beckham Carver
+ * Date: April 11, 2023
+ *
+ * COSC 3750, Homework 7
+ *
+ * Client side of 'Talk' utility on port 51100 that simply echos input of one
+ * machine to other
+ */
+
+#include "socketfun.h"
+#include <stdio.h>
+#include <unistd.h>
+
+#define PORT 51100
+
+
+
+
---
+++ Homework_7/wytalkD.c
@@ -0,0 +1,20 @@
+/*
+ * wytalkD.c
+ * Author: Beckham Carver
+ * Date April 11, 2023
+ *
+ * COSC 3750, Homework 7
+ *
+ * Server/Daemon side of 'Talk' utility, opens port 51100 and accepts connection
+ * from clientside request, echoes input of client
+ *
+ */
+
+#include "socketfun.h"
+#include <unistd.h>
+
+
+
+
+
+
